2b77f930d48d04c7e5ab8e3a42c51f5bc7e79539
Chiara <chiara.muscari@mail.polimi.it>
2021-03-14 13:31:10 +0100

    modified and commented version of myMemory source

--- Keil_data_transfer_axi/main/main.c
+++ Keil_data_transfer_axi/main/main.c
@@ -39,10 +39,10 @@ int main (void)
 {
 
     // Define local variables
-		volatile int     i;
-		u32 axi_memory_data[8] = {0x01234567, 0x89abcdef, 0xdeadbeef, 0xfeebdaed, 0xa5f03ca5, 0x87654321, 0xfedc0ba9, 0x01020408};
-		volatile u32 *pAXImemory = (u32 *) XPAR_MYMEMORY_0_BASEADDR;
-		volatile u32 axi_data_read[8] = {0};
+	volatile int     i;
+	u32 axi_memory_data[8] = {0x01234567, 0x89abcdef, 0xdeadbeef, 0xfeebdaed, 0xa5f03ca5, 0x87654321, 0xfedc0ba9, 0x01020408};
+	volatile u32 *pAXImemory = (u32 *) XPAR_MYMEMORY_0_BASEADDR;
+	volatile u32 axi_data_read[8] = {0};
 
 		
 	  // *****************************************************
@@ -50,14 +50,14 @@ int main (void)
     // *****************************************************
  
     // Write to AXI
-		for(i=0; i<(sizeof(axi_memory_data)/sizeof(u32)); i++)
+	for(i=0; i<(sizeof(axi_memory_data)/sizeof(u32)); i++)
         *pAXImemory++ = axi_memory_data[i];
 
     // Reset the pointer
     pAXImemory = (u32 *)XPAR_MYMEMORY_0_BASEADDR;
-		for(i=0; i<(sizeof(axi_memory_data)/sizeof(u32)); i++)
+	for(i=0; i<(sizeof(axi_memory_data)/sizeof(u32)); i++)
     {
-			axi_data_read[i]=*pAXImemory++;
+		axi_data_read[i]=*pAXImemory++;
     }
 
 }
